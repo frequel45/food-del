1fbb9f7810998d9e6b312fb0ef5bddb78fdf222f
frequel45 <154789727+frequel45@users.noreply.github.com>
2024-06-04 01:57:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const port = 4000
 app.use(express.json())
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://food-del-api-five.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
